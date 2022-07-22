718a476a46983e898b21d53bc87b850f7a9dc4c0
mpryazh <mpryazh@gmail.com>
2022-07-21 20:05:09 -0400

    feat: add mobile menu

---
+++ scripts/mobile_menu.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// mobile menu
+const menu = document.querySelector("#mobile-menu");
+const logo = document.querySelector(".logo");
+const menuLogo = logo.cloneNode(true);
+menuLogo.id = "logo-copy";
+const navigation = document.querySelector("nav ul").cloneNode(true);
+
+menu.append(menuLogo, navigation);
+
+const burger = document.querySelector("#burger");
+burger.addEventListener("click", openMenu);
+
+// close menu when click on links
+const menuLinks = menu.querySelectorAll("a");
+menuLinks.forEach(link => link.addEventListener("click", closeMenu));
+
+// close menu when click outside of menu
+const blackoutScreen = document.querySelector(".blackout");
+blackoutScreen.addEventListener("click", closeMenu);
+
+function openMenu() {
+    // open/close menu
+    if (menu.className == "open") {
+        closeMenu();
+    }
+    else {
+        menu.className = "open";
+        burger.className = "burger-open";
+        document.body.className = "menu-open";
+    }
+}
+function closeMenu() {
+    menu.className = "closed";
+    burger.className = "burger-closed";
+
+    // set delay 0.3s for animation before hiding menu
+    setTimeout(() => menu.className += " hidden", 300);
+
+    document.body.className = "";
+}
+
+
+
+
